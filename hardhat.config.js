e73c75012f05adb50fb8c3d3d28bb3b85cd8f692
DariaDonskaia <donskaia.daria27@gmail.com>
2022-02-14 23:21:04 +0800

    Move tasks scripts in file tasks/task.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-web3");
+require("./tasks/task.js");
 
 const { alchemyApiKey, mnemonic } = require('./secrets.json');
 
@@ -21,7 +22,6 @@ module.exports = {
       }
     }
   },
-
   paths: {
     sources: "./contracts",
     tests: "./test",
@@ -30,46 +30,4 @@ module.exports = {
     task: "./tasks"
   },
 };
-
-//move this code in task.js
-//read https://hardhat.org/advanced/building-plugins.html
-task("make_donations", "Become a donor and make donations")
-  .addParam("money", "Money for donations")
-  .setAction(async (taskArgs, hre) => {
-    const MyContract = await ethers.getContractFactory("Donation");
-    hardhatToken = await MyContract.deploy();  
-    await hardhatToken.setDonor({value: ethers.utils.parseEther(taskArgs.money)});
-    console.log("You send ", taskArgs.money, "You are donor! Thank you!");
-  });
-
-task("get_all_addresses", "Get all addresses that have made transfers")
-  .setAction(async (taskArguments, hre, runSuper) => {
-    const MyContract = await ethers.getContractFactory("Donation");
-    hardhatToken = await MyContract.deploy();  
-    const listAddress = await hardhatToken.getAllDonorsAddress();
-    console.log("Addresses from which the money came: ",listAddress);
-  });
-
-  task("get_amount_donor_money", "The full amount of the amount was transferred from the address")
-  .addParam("address", "The address from which funds were received")
-  .setAction(async (taskArgs) => {
-    const MyContract = await ethers.getContractFactory("Donation");
-    hardhatToken = await MyContract.deploy();  
-    const amount = await hardhatToken.getAmountDonorMoney(taskArgs.address);
-
-    console.log("Full amount from ", taskArgs.address , " :", amount); //search way get 'amount' by the value. idk why amount.value don't work
-  });
-
-  task("transfer_to", "Withdraw money to a specific address")
-  .addParam("address", "The address where the money will be withdrawn")
-  .addParam("money", "The amount to be withdrawn")
-  .setAction(async (taskArgs) => {
-    const MyContract = await ethers.getContractFactory("Donation");
-    hardhatToken = await MyContract.deploy();  
-    await hardhatToken.transferTo(taskArgs.address,taskArgs.amount);
-
-    console.log("The ",taskArgs.money,"was transferred to ",taskArgs.address);
-  });
-
-
 
--- tasks/task.js
+++ tasks/task.js
@@ -1,38 +1,38 @@
 
-
-//https://hardhat.org/advanced/building-plugins.html 
-
 task("make_donations", "Become a donor and make donations")
   .addParam("money", "Money for donations")
   .setAction(async (taskArgs, hre) => {
-
-    await hre.hardhatToken.setDonor({value: ethers.utils.parseEther(taskArgs.money)});
+    const MyContract = await ethers.getContractFactory("Donation");
+    hardhatToken = await MyContract.deploy();  
+    await hardhatToken.setDonor({value: ethers.utils.parseEther(taskArgs.money)});
     console.log("You send ", taskArgs.money, "You are donor! Thank you!");
   });
 
 task("get_all_addresses", "Get all addresses that have made transfers")
-  .setAction(async (taskArguments, hre) => {
-   
-    const listAddress = await hre.hardhatToken.getAllDonorsAddress();
+  .setAction(async (taskArguments, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory("Donation");
+    hardhatToken = await MyContract.deploy();  
+    const listAddress = await hardhatToken.getAllDonorsAddress();
     console.log("Addresses from which the money came: ",listAddress);
   });
 
   task("get_amount_donor_money", "The full amount of the amount was transferred from the address")
   .addParam("address", "The address from which funds were received")
-  .setAction(async (taskArgs, hre) => {
-    
-    const amount = await hre.hardhatToken.getAmountDonorMoney(taskArgs.address);
+  .setAction(async (taskArgs) => {
+    const MyContract = await ethers.getContractFactory("Donation");
+    hardhatToken = await MyContract.deploy();  
+    const amount = await hardhatToken.getAmountDonorMoney(taskArgs.address);
 
-    console.log("Full amount from ", taskArgs.address , " :", amount);
+    console.log("Full amount from ", taskArgs.address , " :", amount); //search way get 'amount' by the value. idk why amount.value don't work
   });
 
   task("transfer_to", "Withdraw money to a specific address")
   .addParam("address", "The address where the money will be withdrawn")
   .addParam("money", "The amount to be withdrawn")
-  .setAction(async (taskArgs, hre) => {
-   
-    await hre.hardhatToken.transferTo(taskArgs.address,taskArgs.amount);
+  .setAction(async (taskArgs) => {
+    const MyContract = await ethers.getContractFactory("Donation");
+    hardhatToken = await MyContract.deploy();  
+    await hardhatToken.transferTo(taskArgs.address,taskArgs.amount);
 
     console.log("The ",taskArgs.money,"was transferred to ",taskArgs.address);
   });
-module.exports = {};
